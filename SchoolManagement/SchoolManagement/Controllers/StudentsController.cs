421bafcb0ab47b2e336a91439ad772e7cdcc8507
Sadman Walid <40543547+SadmanWalid@users.noreply.github.com>
2021-07-31 17:14:12 +0600

    Created Student Controller .
    Created Student Controller.
    *Added Index View.

---
+++ SchoolManagement/SchoolManagement/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManagement.Models;
+
+namespace SchoolManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        private readonly SchoolManagementDBEntities db = new SchoolManagementDBEntities();
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+    }
+}
